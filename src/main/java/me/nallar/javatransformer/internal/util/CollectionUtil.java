63ab8103e48399f6826b5356767b9beaca889cd8
Ross Allan <ross@nallar.me>
2016-01-16 23:42:52 +0000

    Squash varargs warning

--- src/main/java/me/nallar/javatransformer/internal/util/CollectionUtil.java
+++ src/main/java/me/nallar/javatransformer/internal/util/CollectionUtil.java
@@ -5,6 +5,7 @@ import java.util.stream.*;
 
 public class CollectionUtil {
 	@SafeVarargs
+	@SuppressWarnings("varargs")
 	public static <T> Stream<T> union(Collection<T>... collections) {
 		return union(Arrays.asList(collections));
 	}
