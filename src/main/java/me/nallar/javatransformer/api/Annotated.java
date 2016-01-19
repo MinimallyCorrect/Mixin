246ab31a21b22bb245715020af04575ff09ecca5
Ross Allan <rallanpcl@gmail.com>
2016-01-19 12:17:55 +0000

    JavaTransformer is now at https://github.com/nallar/JavaTransformer

--- src/main/java/me/nallar/javatransformer/api/Annotated.java
+++
@@ -1,7 +0,0 @@
-package me.nallar.javatransformer.api;
-
-import java.util.*;
-
-public interface Annotated {
-	List<Annotation> getAnnotations();
-}
--- src/main/java/me/nallar/javatransformer/api/Named.java
+++
@@ -1,7 +0,0 @@
-package me.nallar.javatransformer.api;
-
-public interface Named {
-	String getName();
-
-	void setName(String name);
-}
