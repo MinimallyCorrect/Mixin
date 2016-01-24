8a468ef2d2f8100a323aeff43efbc12f25655612
Luna <git@nyx.nova.fail>
2016-01-24 17:07:57 +0000

    Partially working annotation application pending changes in JavaTransformer

--- src/main/java/me/nallar/mixin/internal/MixinApplicator.java
+++ src/main/java/me/nallar/mixin/internal/MixinApplicator.java
@@ -3,15 +3,20 @@ package me.nallar.mixin.internal;
 import lombok.Data;
 import lombok.NonNull;
 import lombok.val;
+import me.nallar.javatransformer.api.Annotated;
 import me.nallar.javatransformer.api.Annotation;
 import me.nallar.javatransformer.api.ClassInfo;
+import me.nallar.javatransformer.api.MethodInfo;
 
 import java.nio.file.*;
 import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.stream.*;
 
 @Data
 public class MixinApplicator {
+	private static Map<String, Consumer<Annotated>> consumerMap = new HashMap<>();
 	@NonNull
 	private final Path targetJar;
 	@NonNull
@@ -27,22 +32,45 @@ public class MixinApplicator {
 
 	}
 
-	private void handleAnnotations(ClassInfo info) {
-		List<Annotation> mixins = info.getAnnotations(Names.MIXIN_FULL);
+	static {
+		addAnnotationHandler("New", x -> {
+			// TODO: 24/01/2016
+		});
+	}
+
+	private void handleAnnotations(ClassInfo clazz) {
+		List<Annotation> mixins = clazz.getAnnotations(Names.MIXIN_FULL);
 
 		if (mixins.size() == 0)
-			if (noMixinIsError) throw new RuntimeException("Class " + info.getName() + " is not an @Mixin");
+			if (noMixinIsError) throw new RuntimeException("Class " + clazz.getName() + " is not an @Mixin");
 			else return;
 
 		if (mixins.size() > 1)
-			throw new RuntimeException(info.getName() + " can not use @Mixin multiple times");
+			throw new RuntimeException(clazz.getName() + " can not use @Mixin multiple times");
 
 		val mixin = mixins.get(0);
 		String target = (String) mixin.values.get("target");
 
 		if (target == null || target.isEmpty()) {
-			target = info.getSuperType().getClassName();
+			target = clazz.getSuperType().getClassName();
 		}
+
+		logInfo("Found Mixin class '" + clazz.getName() + "' targeting class '" + target);
+		clazz.getMethods().forEach(MixinApplicator::handleAnnotation);
+	}
+
+	@SuppressWarnings("unchecked")
+	private static void addAnnotationHandler(String name, Consumer<? extends Annotated> methodInfoConsumer) {
+		consumerMap.put(name, (Consumer<Annotated>) methodInfoConsumer);
+	}
+
+	private static void handleAnnotation(Annotated annotated) {
+		annotated.getAnnotations().forEach(annotation -> Optional.of(consumerMap.get(annotation.type.getClassName())).ifPresent(x -> x.accept(annotated)));
+	}
+
+	private void logInfo(String s) {
+		// TODO: 24/01/2016 Call
+		System.out.println(s);
 	}
 
 	public static class Names {
@@ -52,5 +80,9 @@ public class MixinApplicator {
 
 		public static String ADD_FULL = PACKAGE + ADD;
 		public static String MIXIN_FULL = PACKAGE + MIXIN;
+	}
+
+	private interface AnnotationApplier {
+		public void apply(Annotation a, Annotated annotatedNode, ClassInfo mixinSource, ClassInfo mixinTarget);
 	}
 }
