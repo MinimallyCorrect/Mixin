82c0eaf8f90fc640057dc38b1240cae322d8bea0
Luna <git@nyx.nova.fail>
2016-01-26 19:07:04 +0000

    Error if @Mixin class is not abstract

--- src/test/java/me/nallar/mixin/internal/mixinsource/MixinSource.java
+++ src/test/java/me/nallar/mixin/internal/mixinsource/MixinSource.java
@@ -5,6 +5,6 @@ import me.nallar.mixin.Mixin;
 import me.nallar.mixin.internal.MixinError;
 
 @Mixin
-public class MixinSource extends MixinError {
+public abstract class MixinSource extends MixinError {
 	private static final long serialVersionUID = 0;
 }
