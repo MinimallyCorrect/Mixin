e658b320ef47b84e6ce33293b901c2c8422a2de5
Luna <git@nyx.nova.fail>
2016-01-31 14:12:26 +0000

    Allow adding multiple sources and getting a single mixin transformer

--- src/test/java/me/nallar/mixin/internal/MixinApplicatorTest.java
+++ src/test/java/me/nallar/mixin/internal/MixinApplicatorTest.java
@@ -9,7 +9,8 @@ public class MixinApplicatorTest {
 	@Test
 	public void testGetMixinTransformer() throws Exception {
 		val applicator = new MixinApplicator();
-		val transformer = applicator.getMixinTransformer(MixinSource.class);
+		applicator.addSource(MixinSource.class);
+		val transformer = applicator.getMixinTransformer();
 
 		Assert.assertTrue("Must have at least one mixin transformer registered", transformer.getClassTransformers().size() != 0);
 	}
