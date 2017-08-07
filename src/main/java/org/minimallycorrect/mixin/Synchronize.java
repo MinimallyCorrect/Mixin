aa469d9ba5f9da1689c206cababbe739b0d9edd6
Ross Allan <ross@nallar.me>
2017-08-07 22:00:33 +0100

    Add Synchronize annotation for adding the synchronized attribute to a method

---
+++ src/main/java/org/minimallycorrect/mixin/Synchronize.java
@@ -0,0 +1,8 @@
+package org.minimallycorrect.mixin;
+
+import java.lang.annotation.*;
+
+@java.lang.annotation.Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Synchronize {
+}
